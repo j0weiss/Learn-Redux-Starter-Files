77f2b515d39dafb50fedd425d0aa415ff59b75de
j0weiss <25281769+j0weiss@users.noreply.github.com>
2017-10-31 17:01:36 +0100

    Setup basic redux actions and reducers

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,11 @@
+// a reducer takes in two things:
+// 1. the action (information about what happened)
+// 2. copy of current state
+
+function posts(state = [], action) {
+  console.log("The post will change.");
+
+  return state;
+}
+
+export default posts;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 
+import store, { history } from './store';
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
@@ -10,12 +12,14 @@ import css from './styles/style.styl'
 
 
 const router = (
-  <Router history={ browserHistory }>
-    <Route path="/" component={ Main }>
-      <IndexRoute component={ PhotoGrid }></IndexRoute>
-      <Route path="/view/:postId" component={ Single }></Route>
-    </Route>
-  </Router>
+  <Provider store={ store }>
+    <Router history={ history }>
+      <Route path="/" component={ Main }>
+        <IndexRoute component={ PhotoGrid }></IndexRoute>
+        <Route path="/view/:postId" component={ Single }></Route>
+      </Route>
+    </Router>
+  </Provider>
 );
 
 render(router, document.getElementById('root'));
