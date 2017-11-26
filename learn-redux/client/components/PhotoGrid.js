02b60d6261e89c48641d0f58c7f8f893284ed86d
j0weiss <weiss_johannes@icloud.com>
2017-11-26 12:41:27 +0100

    Access photos in photo grid and single photo from redux

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Photo from './Photo';
+
+
 class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        I'm the Grid
+        { this.props.posts.map((post, index) => <Photo {...this.props} key={index} index={index} post={post} />) }
       </div>
     )
   }
